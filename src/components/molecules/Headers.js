e82c1fdf678b603bbb3c3c13d8f15f85cd11e851
YSSkywalker <yukimurasakiskywalker940716@gmail.com>
2021-03-26 23:34:06 +0900

    Create Headers

---
+++ src/components/molecules/Headers.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import './Header.css';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header id="header" className="header">
+            </header>
+        );
+    }
+}
+
+export default Header;
